26f457cd29a4d8ca4d8bf0acd384746e82bbb784
Your Name <you@example.com>
2025-05-05 21:49:12 +0400

    alqoritm tasks v1

---
+++ alqoritm/main.js
@@ -0,0 +1,17 @@
+// task-1
+console.log("Ededlerin toplanmasina xosh geldiniz");
+var first = prompt("Zehmet olmasa eded daxil edin");
+var second = prompt("Zehmet olmasa eded daxil edin");
+var sum = Number(first) + Number(second);
+console.log("Ededlerin cemi " + sum);
+// task-2
+alert("Hello World");
+// task-3
+console.log("Ededlerin hasilinin tapilmasina xos geldiniz");
+var reqem1 = prompt("Zehmet olmasa eded daxil edin");
+var reqem2 = prompt("Zehmet olmasa eded daxil edin");
+var cem = Number(reqem1) * Number(reqem2);
+console.log("Ededlerin hasili " + cem);
+// task-4
+var thing = prompt("Bir sheyler yaz");
+alert(thing);
---
+++ alqoritm/main.ts
@@ -0,0 +1,30 @@
+// task-1
+console.log("Ededlerin toplanmasina xosh geldiniz");
+
+let first = prompt ("Zehmet olmasa eded daxil edin");
+
+let second = prompt ("Zehmet olmasa eded daxil edin");
+
+let sum = Number(first) + Number(second);
+
+console.log("Ededlerin cemi " + sum);
+
+// task-2
+alert("Hello World");
+
+// task-3
+console.log("Ededlerin hasilinin tapilmasina xos geldiniz");
+
+let reqem1 = prompt ("Zehmet olmasa eded daxil edin")
+
+let reqem2 = prompt ("Zehmet olmasa eded daxil edin")
+
+let cem = Number(reqem1) * Number(reqem2)
+
+console.log("Ededlerin hasili " + cem)
+
+// task-4
+
+let thing = prompt("Bir sheyler yaz")
+
+alert(thing)
